cd941da35d2c5f5e95e54cef6e933fc68b31bb5d
Spyei <caio.salamao1234@gmail.com>
2024-03-14 10:27:06 -0300

    chore: automatic cat product

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,18 +1,22 @@
-export default function Product({ product, products })  {
+export default function Product({ product, points, onBuy, type })  {
   const handleBuy = () => {
-    const actualCat = localStorage.getItem("actualCat");
-    if (actualCat === product.id) return alert("Você já está com esse gato equipado");
-    const points = localStorage.getItem("poi ts");
-    if (points > product.price) return alert("Você não tem pontos suficientes")
-    localStorage.setItem("points", product.price - points);
-    localStorage.setItem("actualCat", product.id);
-  }
+    onBuy(product);
+  };
+
   return (
-    <button onClick={handleBuy} className="rounded-lg bg-gray-200 flex items-center hover:scale-110 flex-col p-3 break-words">
+    <button onClick={handleBuy} className="rounded-lg bg-gray-200 flex items-center hover:scale-110 flex-col p-3 break-words gap-1 transition-all">
       <h1 className="font-bold text-xl">{product.name}</h1>
-      <img className="rounded-lg" src={product.image} alt={product.name}/>
+      <img className="rounded-lg w-36 h-36" src={product.image} alt={product.name}/>
       <span><strong>{product.price}</strong> pontos</span>
-      <span>{product.points} pontos por clique</span>
+      {type === "click" ? (
+          <>
+            <span>{product.points} pontos por clique</span>
+            <span className="text-start">{product.cooldown} Milisegundos de cooldown</span>
+          </>
+        ) : (
+          <span><strong>{product.points}</strong> ponto{product.points === 1 ? null : "s"} a cada {product.cooldown} milisegundos</span>
+        )
+      }
     </button>
   )
 }
--- src/components/Store.jsx
+++ src/components/Store.jsx
@@ -1,13 +1,19 @@
 import Product from "./Product";
 
-export default function Store({ points, products }) {
+export default function Store({ points, products, onBuy, autocats }) {
   
   return (
-    <section className="flex my-4 items-center flex-col border-2 rounded-lg p-3 w-[90%]">
+    <section className="flex my-4 items-center flex-col border-2 rounded-lg p-6 m-2">
       <h1 className="text-2xl mb-2 font-bold">Loja de gatos</h1>
       <div className="gap-3 grid grid-cols-2">
         {products.map((product) => (
-          <Product products={products} product={product} />
+          <Product key={product.id} product={product} points={points} onBuy={onBuy} type="click" />
+        ))}
+      </div>
+      <h1 className="text-2xl mb-2 my-3 font-bold">Loja de gatos automáticos</h1>
+      <div className="gap-3 grid grid-cols-2">
+        {autocats.map((product) => (
+          <Product key={product.id} product={product} points={points} onBuy={onBuy} type="auto" />
         ))}
       </div>
     </section>
