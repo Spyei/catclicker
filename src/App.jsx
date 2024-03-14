01a4103da991b65a18b95a802b6bc77630f45829
Spyei <caio.salamao1234@gmail.com>
2024-03-14 10:26:34 -0300

    feat: automatic cats

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,26 @@
 import { useState, useEffect } from "react";
 import Store from "./components/Store";
-
-const cats = [
-    {
-      name: "Cozinheiro",
-      image: "https://cdn.discordapp.com/attachments/955900016436838440/1217111130095489064/39df1e09db92ee24b17f5d9e991e10ec.jpg?ex=6602d612&is=65f06112&hm=1f531ad7d4d028c6992514ff6462b06df38765adcfcbbbc1d1c9fd3afc611535&",
-      points: 3,
-      price: 50,
-      id: "cozinheiro",
-      cooldown: 2000
-    },
-    {
-      name: "Banho",
-      image: "https://cdn.discordapp.com/attachments/955900016436838440/1217111651057275053/cd9d104731bdc33161faae5e2a8fe78f.jpg?ex=6602d68e&is=65f0618e&hm=ef8d5c6d30f40b9f909c31f866104796deb9dcb000a8913a5660e4a936fac15d&",
-      points: 6,
-      price: 100,
-      id: "banho",
-      cooldown: 2000
-    },
-    {
-      name: "Inicial",
-      image: "https://cdn.discordapp.com/attachments/955900016436838440/1216747144308854827/86ad1a9e359f4970e48741dd1ffde549.jpg?ex=66018315&is=65ef0e15&hm=bcc54bfc0e1742ddb4aa6d109062e3dbaf247236a53497d00323773424bf4deb&",
-      points: 1,
-      price: 0,
-      id: "gatolegal",
-      cooldown: 2000
-    }
-  ];
+import { cats, catsauto } from "./utils";
 
 export default function App() {
   const [count, setCount] = useState(parseFloat(localStorage.getItem("points")) || 0);
+  const [cat, setCat] = useState(cats.find((cat) => cat.id === (localStorage.getItem("actualCat") || "gatolegal")));
+  const [automaticCat, setAutomaticCat] = useState(catsauto.find((autocat) => autocat.id === localStorage.getItem("autoCat")));
   const [click, setClick] = useState(false);
+
   useEffect(() => {
     localStorage.setItem("points", count);
   }, [count]);
 
-  const cat = cats.find((cat) => cat.id === (localStorage.getItem("actualCat") || "gatolegal"));
+  useEffect(() => {
+    if (automaticCat) {
+      const interval = setInterval(() => {
+        setCount(prevCount => prevCount + automaticCat.points);
+      }, automaticCat.cooldown);
+
+      return () => clearInterval(interval);
+    }
+  }, [automaticCat]);
 
   function handleAddPoints() {
     setClick(!click);
@@ -43,15 +28,34 @@ export default function App() {
       setCount(count + cat.points);
     }, cat.cooldown);
   }
+
+  function handleBuy(product) {
+    if (catsauto.map((a) => a.id).includes(product.id)) {
+      if (count < product.price) return alert("Você não tem pontos suficientes");
+
+      setCount(count - product.price);
+      setAutomaticCat(product);
+      localStorage.setItem("autoCat", product.id);
+    } else {
+      if (count < product.price) return alert("Você não tem pontos suficientes");
+
+      setCount(count - product.price);
+      setCat(product);
+      localStorage.setItem("actualCat", product.id);
+    }
+  }
   
   return (
     <section className="flex flex-col w-screen items-center">
-    <h1 className="font-bold text-2xl">Clique no gato</h1>
+      <h1 className="font-bold text-2xl">Clique no gato</h1>
       <button className="flex items-center flex-col my-3" onClick={handleAddPoints}>
-        <img src={cat.image} alt="Cat image" className={`rounded-lg transition-all ${click ? "scale-90" : null} h-1/2 w-1/2`}/>
+        <img src={cat.image} alt="Cat image" className={`rounded-lg transition-all ${click ? "scale-90" : null} h-1/2 w-1/2`} />
       </button>
-      <div>Pontos: <strong>{count}</strong></div>
-      <Store products={cats} points={count} />
+      <div className="w-full flex text-center flex-col">
+        <div>Pontos: <strong>{count}</strong></div>
+        <span>{cat.points} ponto{cat.points === 1 ? null : "s"} a cada <strong>{cat.cooldown} milisegundos</strong> por click</span>
+      </div>
+      <Store autocats={catsauto} products={cats} points={count} onBuy={handleBuy} />
     </section>
   )
 }
---
+++ src/utils/index.ts
@@ -0,0 +1,69 @@
+export const cats = [
+  {
+    name: "Zolhos",
+    image: "https://cdn.discordapp.com/attachments/955900016436838440/1217111650038186106/6a0f2fa39ee9953174c804f4285bc81f.jpg?ex=6602d68e&is=65f0618e&hm=15592476fa51a39f88fc4d3f3262f43810b3a9e73dddf09cb071f071b7ba69d4&",
+    points: 24,
+    price: 160 + 160,
+    cooldown: 1500,
+    id: "zolhos"
+  },
+  {
+    name: "Surpreso",
+    image: "https://cdn.discordapp.com/attachments/955900016436838440/1217111651661254697/e06443b9439ca8611248d17cad479892.jpg?ex=6602d68e&is=65f0618e&hm=83ed54d2397b0d745c5ab50bd824b33246c87a5133c8878ffee00d354f75e2d3&",
+    points: 12,
+    price: 160,
+    id: "surpreso",
+    cooldown: 1800
+  },
+  {
+    name: "Banho",
+    image: "https://cdn.discordapp.com/attachments/955900016436838440/1217111651057275053/cd9d104731bdc33161faae5e2a8fe78f.jpg?ex=6602d68e&is=65f0618e&hm=ef8d5c6d30f40b9f909c31f866104796deb9dcb000a8913a5660e4a936fac15d&",
+    points: 6,
+    price: 70,
+    id: "banho",
+    cooldown: 2000
+  },
+  {
+    name: "Cozinheiro",
+    image: "https://cdn.discordapp.com/attachments/955900016436838440/1217111130095489064/39df1e09db92ee24b17f5d9e991e10ec.jpg?ex=6602d612&is=65f06112&hm=1f531ad7d4d028c6992514ff6462b06df38765adcfcbbbc1d1c9fd3afc611535&",
+    points: 3,
+    price: 35,
+    id: "cozinheiro",
+    cooldown: 2000
+  },
+  {
+    name: "Inicial",
+    image: "https://cdn.discordapp.com/attachments/955900016436838440/1216747144308854827/86ad1a9e359f4970e48741dd1ffde549.jpg?ex=66018315&is=65ef0e15&hm=bcc54bfc0e1742ddb4aa6d109062e3dbaf247236a53497d00323773424bf4deb&",
+    points: 500,
+    price: 0,
+    id: "gatolegal",
+    cooldown: 2000
+  }
+];
+
+export const catsauto = [
+  {
+    name: "AutoGuitaGato",
+    image: "https://cdn.discordapp.com/attachments/955900016436838440/1217797584844754976/0630d69404abb71c243f3bf7c64dec58.jpg?ex=66055561&is=65f2e061&hm=d095a75ce66bba7ea164bdf17633fd2fd55ae403db852e04eefa628b1fa5a705&",
+    points: 50,
+    price: 500,
+    id: "gatoguitarra",
+    cooldown: 10000
+  },
+  {
+    name: "AutoNerdGato",
+    image: "https://cdn.discordapp.com/attachments/955900016436838440/1217814508739760220/b129c3c44135603f0ef80f8a99dc78e1.jpg?ex=66056524&is=65f2f024&hm=ffbcfdf59725512b76806dd58eaad63e1b75b6f1a5b5e36d8f1fc7ef21d31054&",
+    points: 150,
+    price: 975,
+    id: "gatonerd",
+    cooldown: 10000
+  },
+  {
+    name: "AutoTadorGato",
+    image: "https://cdn.discordapp.com/attachments/955900016436838440/1217814508974772224/3a64841d1aeeafad933f6cb7a3efc467.jpg?ex=66056524&is=65f2f024&hm=2d716a94387af7e30fa380cc03e7f3585fe6c562bcf0a3fc5a87c88a1d21daad&",
+    points: 150,
+    price: 950,
+    id: "gatocomputador",
+    cooldown: 5000
+  }
+];
